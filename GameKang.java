816fab0daa4fcb7af4b3915dd7a84c2fb1e0f470
Kkangjn <135511684+Kkangjn@users.noreply.github.com>
2023-06-14 20:14:41 +0900

    주석 생성

--- GameKang.java
+++ GameKang.java
@@ -9,18 +9,18 @@ public class GameKang {
         ArrayList<Integer> computerNumList = new ArrayList<Integer>();
 
         ArrayList<Integer> randomNumber = new ArrayList<Integer>();
-        for (int i=0; i<=9; i++) {
+        for (int i=0; i<=9; i++) { // 배열 생성 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
             randomNumber.add(i);
         }
 
         for (int i=0;i<=2;){
             int j = 10-i;
-            int randomNum = random.nextInt(j);
-            computerNumList.add(randomNumber.get(randomNum));
-            randomNumber.remove(randomNum);
+            int randomNum = random.nextInt(j); // 랜덤 정수 하나 생성
+            computerNumList.add(randomNumber.get(randomNum)); // 랜덤정수로 배열 [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] 에서 하나 뽑기
+            randomNumber.remove(randomNum); // 뽑은 배열 인덱스 삭제
             i++;
         }
-        System.out.println(computerNumList.toString());
+//        System.out.println(computerNumList.toString()); // 컴퓨터 생성 출력()
         System.out.println("컴퓨터가 숫자 생성 완료! 답을 맞춰보세요!");
 
 
@@ -31,17 +31,17 @@ public class GameKang {
             Scanner sc = new Scanner(System.in);
             ArrayList<Integer> userNumList = new ArrayList<Integer>();
             createUserNum:
-            for (int i = 1; i <= computerNumList.size(); ) {
+            for (int i = 1; i <= computerNumList.size(); ) { //유저 입력
                 System.out.println(i+"번째 숫자를 입력하세요");
-                int userNum = sc.nextInt();
+                int userNum = sc.nextInt(); // Scanner 로 받음
 
-                if (userNum>=10){
+                if (userNum>=10){ // 잘못된값 예외처리
                     System.out.println("잘못된 입력입니다.");
                     continue createUserNum;
                 }
 
                 for (int j : userNumList) {
-                    if (j == userNum) {
+                    if (j == userNum) { // 중복값 처리
                         System.out.println("중복된 값입니다.");
                         continue createUserNum;
                     }
@@ -54,12 +54,12 @@ public class GameKang {
             System.out.println(x+"번째 시도: "+userNumList.toString());
             int strike = 0;
             int ball = 0;
-            for (int i = 0; i < userNumList.size(); i++) {
-                int checkUserNum = userNumList.get(i);
-                for (int j = 0; j < computerNumList.size(); j++) {
+            for (int i = 0; i < userNumList.size(); i++) { // 값 비교부분
+                int checkUserNum = userNumList.get(i); // 유저 인덱스 1, 2, 3
+                for (int j = 0; j < computerNumList.size(); j++) { // 유저인덱스를 각각 랜덤숫자 1, 2, 3 과 비교
                     int checkComputerNum = computerNumList.get(j);
                     if (checkComputerNum == checkUserNum) {
-                        if (i == j) {
+                        if (i == j) { // 숫자 자리 비교
                             strike++;
                         } else {
                             ball++;
@@ -76,7 +76,7 @@ public class GameKang {
                 break replay;
             } else {
                 System.out.println(ball + "B" + strike + "S");
-                continue replay;
+                continue replay; // 입력부터 반복!
             }
         }
     }
